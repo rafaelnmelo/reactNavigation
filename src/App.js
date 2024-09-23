37d1ea9a03d4e55b7d02825a1bcafdb21fd16aa5
Rafael Melo <rafaelnunesmelo@gmail.com>
2024-09-23 12:49:01 -0300

    Criando primeiros componentes

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { SafeAreaView } from "react-native"
+import TextoCentral from "./components/TextoCentral"
+
+
+export default props => (
+    <SafeAreaView style={{flex: 1}}>
+        <TextoCentral>APP</TextoCentral>
+    </SafeAreaView>
+)
