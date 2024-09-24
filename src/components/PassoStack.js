a829302abad111754a8f48149317857e5439cf7d
Rafael Melo <rafaelnunesmelo@gmail.com>
2024-09-24 12:53:25 -0300

    Ajustes formatação

--- src/components/PassoStack.js
+++ src/components/PassoStack.js
@@ -2,31 +2,32 @@ import React from 'react'
 import { View, Button } from 'react-native'
 
 export default props => (
-    <View style={{flex: 1}}>
-        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+    <View style={{ flex: 1 }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
             {props.voltar
-                ?   <Button
-                        title='Voltar'
-                        onPress={() => {
-                            props.navigation.goBack()
-                        }}
-                    />
+                ? <Button
+                    title='Voltar'
+                    onPress={() => {
+                        props.navigation.goBack()
+                    }}
+                />
                 : false
             }
             {props.avancar
-                ?   <Button
-                        title='Avançar'
-                        onPress={() => {
-                            props.navigation.navigate(
-                                props.avancar,
-                                props.avancarParams
-                            )
-                        }}
-                    />
+                ? <Button
+                    title='Avançar'
+                    onPress={() => {
+                        //push consegue chamar várias vezes
+                        props.navigation.navigate(
+                            props.avancar,
+                            props.avancarParams
+                        )
+                    }}
+                />
                 : false
             }
         </View>
-        <View style={{flex: 1}}>
+        <View style={{ flex: 1 }}>
             {props.children}
         </View>
     </View>
